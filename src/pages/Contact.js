2a2fd6ddedb2183a5d6f876dabc5a6c024b97aa6
Mahdi550i <mahdibdeir20@gmail.com>
2025-11-26 06:31:21 +0200

    Add images folder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
 
         <input
           className="w-full border rounded px-3 py-2"
-          placeholder="Email"
+          placeholder="Email "
         />
 
         <textarea
